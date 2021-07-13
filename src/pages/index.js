cff282e2aa173474d48c4a4c70606bbb3444403c
TobiBrady <tobias.m.brady@gmail.com>
2021-07-13 14:33:03 +0100

    set up tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
+import '../styles/global.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className="text-red-500 text-xl">Hello world is tailwind working?</div>
 }
