a3bddf3f2ff25517420e8e578d517973a3aa05d4
TobiBrady <tobias.m.brady@gmail.com>
2021-07-29 19:02:17 +0100

    link removed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { Link } from "gatsby"
 import '../styles/global.css'
 import trooper from '../../static/404.png'
 
@@ -16,7 +15,7 @@ export default function Lost() {
             <img src={ trooper } alt="move along" className="m-auto max-h-96" />
 
             <div className="text-center mb-auto text-black font-bold ">
-                <Link to="/" className="hover:text-blue-500">Click here to go about your buisness.</Link>
+                <a herf="/" className="hover:text-blue-500">Click here to go about your buisness.</a>
             </div>
 
         </div>
